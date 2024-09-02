62baffc4484eea8d4a66b70c33b254c9836c9625
-m <-mkhalidhissoune962@gmail.com>
2024-09-02 21:18:54 +0100

    add the regex
    i added the logic of validation of the inputs

--- index.js
+++ index.js
@@ -9,7 +9,16 @@
 const PlayersHistory = JSON.parse(localStorage.getItem("PlayersHistory")) || [];
     submitButton.addEventListener("click", function() {
     const firstname = document.getElementById("firstname").value;
-    const secondname = document.getElementById("secondname").value;
+        const secondname = document.getElementById("secondname").value;
+        const nameRegex = /^[A-Za-z\s]+$/;
+  if (!nameRegex.test(firstname)) {
+        alert("Please enter a valid first name. Only letters and spaces are allowed.");
+        return;
+        }
+        if (!nameRegex.test(secondname)) {
+        alert("Please enter a valid second name. Only letters and spaces are allowed.");
+        return;
+    }
         // console.log(firstname);
         if (firstname && secondname) {
            const Players = {
